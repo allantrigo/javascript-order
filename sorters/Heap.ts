a30891b790ff0ec976bc9099a87cd9fa25a9e051
Allan Trigo <allantrigo@gec.inatel.br>
2022-06-02 02:07:42 -0300

    feat: fix formatting

--- sorters/Heap.ts
+++ sorters/Heap.ts
@@ -1,56 +1,54 @@
-import { Sort } from "./Sort"
+export const Heap = function () {
 
-
-export class Heap implements Sort {
-    maxHeap(array: number[]) {
-        let pivot = Math.floor(array.length / 2 - 1)
+    const maxHeap = (array: number[]) => {
+        let pivot = Math.floor(array.length / 2 - 1);
         while (pivot >= 0) {
-            this.heapify(array, pivot, array.length)
-            pivot -= 1
+            heapify(array, pivot, array.length);
+            pivot -= 1;
         }
     }
 
-    heapify(array: number[], pivot: number, max: number) {
-        let index: number
-        let left: number
-        let right: number
+    const heapify = (array: number[], pivot: number, max: number) => {
+        let index: number;
+        let left: number;
+        let right: number;
 
         while (pivot < max) {
-            index = pivot
-            left = 2 * pivot + 1
-            right = left + 1
+            index = pivot;
+            left = 2 * pivot + 1;
+            right = left + 1;
             if (left < max && array[left] > array[index]) {
-                index = left
+                index = left;
             }
             if (right < max && array[right] > array[index]) {
-                index = right
+                index = right;
             }
             if (index === pivot) {
-                return
+                return;
             }
-            this.swap(array, pivot, index)
-            pivot = index
+            swap(array, pivot, index);
+            pivot = index;
         }
     }
 
-    swap(array: number[], first: number, last: number) {
-        const temporary = array[first]
-        array[first] = array[last]
-        array[last] = temporary
+    const swap = (array: number[], first: number, last: number) => {
+        const temporary = array[first];
+        array[first] = array[last];
+        array[last] = temporary;
     }
 
-    heapSort(array: number[]) {
-        this.maxHeap(array)
-        let last = array.length - 1
+    const heapSort = (array: number[]) => {
+        maxHeap(array);
+        let last = array.length - 1;
         while (last > 0) {
-            this.swap(array, 0, last)
-            this.heapify(array, 0, last)
-            last -= 1
+            swap(array, 0, last);
+            heapify(array, 0, last);
+            last -= 1;
         }
-        return array
+        return array;
     }
 
-    sort(array: number[]) {
-        return this.heapSort(array)
+    this.sort = (array: number[]) => {
+        return heapSort(array);
     }
-}
+};
